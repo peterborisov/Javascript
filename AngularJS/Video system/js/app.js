0ecb75ef31b979553f591d50433a2c66cb793740
peterborisov <borisov.peter@yahoo.co.uk>
2016-04-04 13:57:30 +0300

    Add some functionality to Video System

--- AngularJS/Video system/js/app.js
+++ AngularJS/Video system/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('video', []);
 app.controller('videoDetails', function ($scope) {
-    $scope.details = [
+    $scope.video = [
         {
             title: 'Course introduction',
             pictureUrl: 'http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png',
@@ -22,27 +22,12 @@ app.controller('videoDetails', function ($scope) {
                     content: 'Congratulations Nakov',
                     date: new Date(2014, 12, 15, 12, 30, 0),
                     likes: 3,
-                    websiteUrl: 'http://pesho.com/'
-                }
-            ]
-        },
-        {
-            title: 'Angular introduction',
-            pictureUrl: 'http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png',
-            length: '6:32',
-            category: 'Javascript',
-            subscribers: 2,
-            date: new Date(2016, 12, 15),
-            haveSubtitles: true,
-            comments: [
-                {
-                    username: 'Gosgo Peshev',
-                    content: 'Congratulations Softuni',
-                    date: new Date(2014, 12, 15, 12, 30, 0),
-                    likes: 2,
-                    websiteUrl: 'http://gosho.com/'
+                    websiteUrl: 'http://minka.com/'
                 }
             ]
         }
     ]
+    $scope.mySortFunction = function(item) {
+            return item[$scope.title];
+    }
 });
