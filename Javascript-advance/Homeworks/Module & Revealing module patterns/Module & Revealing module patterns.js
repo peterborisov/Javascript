69bfc04fc69e5e3e5d8b6ec82f1b01b46312acae
peterborisov <borisov.peter@yahoo.co.uk>
2016-05-18 08:52:38 +0300

    add module patterns

---
+++ Javascript-advance/Homeworks/Module & Revealing module patterns/Module & Revealing module patterns.js
@@ -0,0 +1,35 @@
+var app = (function () {
+    var data = [];
+
+    function add(obj) {
+        data.push(obj);
+        return data.slice();
+    }
+
+    function plus(a, b) {
+        return console.log(a + b);
+    }
+
+    function minus(a, b) {
+        return a - b;
+    }
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+    return {
+        plus: plus,
+        minus: minus,
+        multiply: multiply,
+        add: add
+    }
+})();
+
+app.plus(5, 4);
+ var m = app.multiply(5, 4);
+var p = app.add('pete');
+
+console.log(m);
+console.log(app.minus(5, 4));
+console.log(p);
