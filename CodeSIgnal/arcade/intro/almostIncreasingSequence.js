89a44b99376aa527c1c6127ec60bb19759b8fbb6
peterborisov <borisov.peter@yahoo.co.uk>
2019-02-20 21:31:37 +0200

    new tasks

---
+++ CodeSIgnal/arcade/intro/almostIncreasingSequence.js
@@ -0,0 +1,29 @@
+// Given a sequence of integers as an array, 
+// determine whether it is possible to obtain a strictly 
+// increasing sequence by removing no more than one element from the array.
+
+function almostIncreasingSequence(sequence) {
+    let found = false;
+    for (let i = 0; i < sequence.length; i++) {
+        if (sequence[i] <= sequence[i - 1]) {
+            if (found) {
+                return false;
+            }
+            found = true;
+
+            if (i === 1 || i + 1 === sequence.length) {
+                continue;
+            }
+            else if (sequence[i] > sequence[i - 2]) {
+                sequence[i - 1] = sequence[i - 2];
+            }
+            else if (sequence[i - 1] >= sequence[i + 1]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+let input = [1, 3, 2, 1];
+almostIncreasingSequence(input)
