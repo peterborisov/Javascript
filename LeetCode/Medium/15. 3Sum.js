c30cef60a70995e456d139fc4205b2abd3c7313d
peterborisov <borisov.peter@yahoo.co.uk>
2020-11-06 21:01:42 +0200

    LeetCode solutions 15

--- LeetCode/Medium/15. 3Sum.js
+++ LeetCode/Medium/15. 3Sum.js
@@ -2,71 +2,29 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
+var threeSum = function (nums) {
     let result = [];
     let sum = 0;
-	let sorted_arr = nums.sort((a,b)=>{
-		return a-b;
-	})
-	for(let i = 0; i<sorted_arr.length;i++){
-        if(i-1>=0 && nums[i] == nums[i-1]){continue}
-		let j = i+1;
-		let k = sorted_arr.length-1;
-		while(j<k){
-			if(sorted_arr[i]+sorted_arr[j]+sorted_arr[k]===sum){
-				result.push([sorted_arr[i],sorted_arr[j],sorted_arr[k]]);
-				j++;
-				while(j<k && sorted_arr[j]===sorted_arr[j-1]) { j++ }
-			} else if(sorted_arr[i]+sorted_arr[j]+sorted_arr[k] < sum){
-				j++;
-			} else {
-				k--;
-			} 
-		}
-	}
-	return result
-    
+    let sorted_arr = nums.sort((a, b) => {
+        return a - b;
+    })
+    for (let i = 0; i < sorted_arr.length; i++) {
+        if (i - 1 >= 0 && nums[i] == nums[i - 1]) { continue }
+        let j = i + 1;
+        let k = sorted_arr.length - 1;
+        while (j < k) {
+            if (sorted_arr[i] + sorted_arr[j] + sorted_arr[k] === sum) {
+                result.push([sorted_arr[i], sorted_arr[j], sorted_arr[k]]);
+                j++;
+                while (j < k && sorted_arr[j] === sorted_arr[j - 1]) { j++ }
+            } else if (sorted_arr[i] + sorted_arr[j] + sorted_arr[k] < sum) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    return result
 };
-// var threeSum = function (nums) {
-//     nums.sort((a, b) => a - b)
-//     let result = [];
-//     let sum = 0;
-
-//     nums.map((el, index) => {
-//         // if (index - 1 >= 0 && el == nums[index - 1]) {  }
-//         let j = index + 1;
-//         let k = nums.length - 1;
-
-//         while (j < k) {
-//             if (index + nums[j] + nums[k] === sum) {
-//                 result.push([index, nums[j], nums[k]]);
-//                 j++;
-//                 while (j < k && nums[j] === nums[j - 1]) { j++ }
-//             } else if (index + nums[j] + nums[k] < sum) {
-//                 j++;
-//             } else {
-//                 k--;
-//             }
-//         }
-//     })
-
-//     // for (let i = 0; i < nums.length; i++) {
-//     // if (i - 1 >= 0 && nums[i] == nums[i - 1]) { continue }
-//     // let j = i + 1;
-//     // let k = nums.length - 1;
-//     // while (j < k) {
-//     //     if (nums[i] + nums[j] + nums[k] === sum) {
-//     //         result.push([nums[i], nums[j], nums[k]]);
-//     //         j++;
-//     //         while (j < k && nums[j] === nums[j - 1]) { j++ }
-//     //     } else if (nums[i] + nums[j] + nums[k] < sum) {
-//     //         j++;
-//     //     } else {
-//     //         k--;
-//     //     }
-//     // }
-//     // }
-//     return result
-// };
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4]))
