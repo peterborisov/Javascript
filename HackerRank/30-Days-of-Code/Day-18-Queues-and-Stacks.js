f49543428529519ec4295a995720b7c66f3aea2c
peterborisov <borisov.peter@yahoo.co.uk>
2020-11-18 17:37:16 +0200

    30 days of code Day-18

--- HackerRank/30-Days-of-Code/Day-18-Queues-and-Stacks.js
+++ HackerRank/30-Days-of-Code/Day-18-Queues-and-Stacks.js
@@ -1,6 +1,11 @@
-function Solution(str) {
-    let reversed = str.split('').reverse().join('')
-    return str === reversed;
+function Solution() {
+    this.stack = [];
+    this.queue = [];
+
+    this.pushCharacter = (ch) => this.stack.push(ch); 
+    this.enqueueCharacter = (ch) => this.queue.push(ch);
+    this.popCharacter = () => this.stack.pop();
+    this.dequeueCharacter = () => this.queue.shift();
 }
 
 console.log(Solution('racecar'))
