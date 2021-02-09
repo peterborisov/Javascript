bd9c163144cfd58cf2c77d761f7848c09de488dc
peterborisov <borisov.peter@yahoo.co.uk>
2021-02-09 08:53:36 +0200

    Prototype chain task

---
+++ Softuni/JS-advanced-2020/18.Exercises-Prototype-chain/3.Posts.js
@@ -0,0 +1,14 @@
+class Post{
+    constructor(title, content){
+        this.title = title;
+        this.content = content;
+    }
+
+    toString(){
+        return `Post: ${this.title} Content: ${this.content}`
+    }
+}
+
+class SocialMediaPost{
+    
+}
